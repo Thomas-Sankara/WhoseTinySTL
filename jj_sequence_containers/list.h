6dcc1bade14c0b646d2395348de7d26340774119
handsomechar <1204209093@qq.com>
2021-06-14 21:36:58 +0800

    尤其注意最后sort的注释

--- jj_sequence_containers/list.h
+++ jj_sequence_containers/list.h
@@ -91,8 +91,8 @@ protected:
     }
 
     // 将[first,last)内的所有元素移动到position之前。直接看代码有点懵每步在干啥，看书139页
-    void transfer(iterator position, iterator first, iterator last){
-        if (position != last) {
+    void transfer(iterator position, iterator first, iterator last){ // 这个功能很常用，就被实现在protected里了
+        if (position != last) { // 比如公开接口（方法）splice的各种重载版本、merge()、reverse()、sort()都调用了它
             (*(link_type((*last.node).prev))).next = position.node;
             (*(link_type((*first.node).prev))).next = last.node;
             (*(link_type((*position.node)))).next = first.node;
@@ -179,5 +179,95 @@ public:
                 first = next; // 调整指针
             next = first; // 修正区段范围
         }
+    }
+
+    // 将x接合于position所指位置之前。x必须不同于*this
+    void splice(irerator position, list& x) {
+        if(!x.empty())
+            transfer(position, x.begin(), x.end());
+    }
+
+    // 将i所指元素接合于position所指位置之前。position和i可指向同一个list
+    void splice(iterator position, list&, iterator i){
+        iterator j = i;
+        ++j;
+        if(position == i || position == j) return;
+        transfer(position, i, j);
+    }
+
+    // 将[first,last)内的所有元素接合于position所指位置之前
+    // position和[first,last)可指向同一个list，
+    // 但position不能位于[first,last)之内
+    void splice(iterator position, list&, iterator first, iterator last){
+        if (first != last)
+            transfer(position, first, last);
+    }
+
+    // merge()将x合并到*this身上。两个lists的内容都必须先经过递增排序
+    template <class T, class Alloc>
+    void list<T, Alloc>::merge(list<T, Alloc>& x){
+        iterator first1 = begin();
+        iterator last1 = end();
+        iterator first2 = x.begin();
+        iterator last2 = x.end();
+
+        // 注意：前提是，两个lists都已经过递增排序
+        while(first1 != last1 && first2 != last2)
+            if(*first2 < *first1){
+                iterator next = first2;
+                transfer(first1, first2, ++next);
+                first2 = next;
+            }else
+                ++first1;
+        if(first2 != last2) transfer(last1, first2, last2);
+    }
+
+    // reverse()将*this的内容逆向重置
+    template <class T, class Alloc>
+    void list<T, ALlloc>::reverse(){
+        // 以下判断，如果是空链表，或仅有一个元素，就不进行任何操作
+        // 使用size() == 0 || size() == 1来判断，虽然也可以，但是比较慢
+        if(node->next == node || link_type(node->next)->next == node) return;
+        iterator first = begin();
+        ++first;
+        while (first != end()) {
+            iterator old = first;
+            ++first;
+            transfer(begin(), old, first);
+        }
+    }
+
+    // list不能使用STL算法sort()，必须使用自己的sort()成员函数，
+    // 因为STL算法sort()只接受RandomAccessIterator（侯捷此处笔误，误写成Ramdon）
+    // 本函数采用quick sort（错！这显然是归并排序！都疯狂merge了咋还quick sort呢！）
+    // 该归并排序的解析参见https://blog.csdn.net/ww32zz/article/details/50282257
+    // 以下代码中的注释除了来自侯捷，剩下的大部分来自该博客，我自己补充了一部分。
+    template <class T, class Alloc>
+    void list<T, Alloc>::sort() {
+        // 以下判断，如果是空链表，或仅有一个元素，就不进行任何操作
+        // 使用size() == 0 || size() == 1来判断，虽然也可以，但是比较慢
+        if(node->next  == node || link_type(node->next)->next == node) return;
+
+        // 一些新的lists，作为中介数据存放区。注意，只占用了常数空间，而且不大。
+        // 这种用遍历而非递归实现的归并排序将list的长度表示为二进制形式。
+        // 用类似加法进位的思想，每次从链表中拿出一个元素，与这些子序列进行归并，
+        // 产生进位则与下一个子序列进行归并，一直到没有进位的产生。
+        list<T, Alloc> carry;       // 加法过程中保存中间结果
+        list<T, Alloc> counter[64]; // 存放不同长度的子序列，每个自序列本身有序
+        int fill = 0;               // 当前二进制位数
+        while (!empty()) {          // 有了前面splice和merge的代码你知道，它们都没有引入新的内存，只是改指针
+            carry.splice(carry.begin(), *this, begin()); // 将list的表头元素转入carry中，相当于+1操作
+            int i = 0;                              // 处理当前的二进制位数
+            while(i<fill && !counter[i].empty()){   // 处理+1产生的进位
+                counter[i].merge(carry);            // 进位。由于merge调用了transfer，执行完这行后carry里面是空的。
+                carry.swap(counter[i++]);           // 保存目前的加法结果，准备处理下一位。这行把计算结果又存入carry，而counter[i]现在被swap成空的了。
+            }                                       // 循环结束时，要么没有进位，要么已经处理到最高位
+            carry.swap(counter[i]);                 // 更新当前子序列，操作后carry为空
+            if(i == fill) ++fill;
+        }
+
+        for (int i = 1; i < fill; ++i) // 子序列归并
+            counter[i].merge(counter[i-1]);
+        swap(counter[fill-1]);
     }
 }
