1cd20d80ffc72cc907316105cda5b05aa657eb20
handsomechar <1204209093@qq.com>
2021-06-18 15:57:20 +0800

    改了一个字母

--- Iterator.h
+++ Iterator.h
@@ -70,7 +70,7 @@ namespace WhoseTinySTL{
     {
         typedef typename Iterator::iterator_category    iterator_category;
         typedef typename Iterator::value_type           value_type;
-        typedef typename Iterator::differenct_type      difference_type;
+        typedef typename Iterator::difference_type      difference_type;
         typedef typename Iterator::pointer              pointer;
         typedef typename Iterator::reference            reference;
     };
